bf8d1ef2279596d512c6ae529f995310b61aec10
dpdlfjsfh <dpdlfjsfh@naver.com>
2023-06-01 13:34:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ async def filter_jewelry(
 
     return filtered_items
 
+#가상으 자동차 렌트 상품 데이터
 car_rental_data = [
     {"vehicle_type": "SUV", "model": "Kia Sportage", "mileage": 10.5, "daily_rental_fee": 90000, "available": True, "location": "서울특별시 강남구"},
     {"vehicle_type": "Sedan", "model": "Hyundai Sonata", "mileage": 12.3, "daily_rental_fee": 80000, "available": True, "location": "서울특별시 마포구"},
@@ -88,3 +89,209 @@ async def filter_car_rental(
         filtered_items = [item for item in filtered_items if item["available"] == available]
 
     return filtered_items
+
+#가상의 SNS 계정 정보 DB
+
+sns_accounts = [
+    {"user_id": "user1", "username": "John Doe", "followers": 500, "following": 300, "posts": 100, "likes": 2000, "comments": 500, "bio": "Passionate photographer"},
+    {"user_id": "user2", "username": "Jane Smith", "followers": 1000, "following": 500, "posts": 200, "likes": 4000, "comments": 1000, "bio": "Food lover and travel enthusiast"},
+    {"user_id": "user3", "username": "David Johnson", "followers": 800, "following": 600, "posts": 150, "likes": 3000, "comments": 800, "bio": "Fitness freak and adventure seeker"},
+    {"user_id": "user4", "username": "Emily Davis", "followers": 1200, "following": 900, "posts": 180, "likes": 5000, "comments": 1200, "bio": "Bookworm and aspiring writer"},
+    {"user_id": "user5", "username": "Michael Wilson", "followers": 300, "following": 200, "posts": 50, "likes": 1000, "comments": 300, "bio": "Music lover and concert goer"},
+    {"user_id": "user6", "username": "Sarah Brown", "followers": 700, "following": 400, "posts": 120, "likes": 2500, "comments": 600, "bio": "Fashion enthusiast and beauty junkie"},
+    {"user_id": "user7", "username": "Christopher Lee", "followers": 1500, "following": 1200, "posts": 250, "likes": 6000, "comments": 1500, "bio": "Tech geek and gadget lover"},
+    {"user_id": "user8", "username": "Olivia Taylor", "followers": 900, "following": 700, "posts": 160, "likes": 3500, "comments": 900, "bio": "Nature lover and outdoor adventurer"},
+    {"user_id": "user9", "username": "Daniel Martin", "followers": 400, "following": 250, "posts": 80, "likes": 1500, "comments": 400, "bio": "Coffee addict and early riser"},
+    {"user_id": "user10", "username": "Sophia Anderson", "followers": 600, "following": 350, "posts": 100, "likes": 2000, "comments": 600, "bio": "Art lover and creative soul"}
+]
+
+@app.get("/sns_accounts")
+async def filter_sns_accounts(
+    min_followers: int = Query(default=None, ge=0),
+    min_following: int = Query(default=None, ge=0),
+    min_likes: int = Query(default=None, ge=0),
+    user_id: str = Query(default=None),
+    username: str = Query(default=None),
+):
+    filtered_accounts = sns_accounts
+
+    if min_followers is not None:
+        filtered_accounts = [account for account in filtered_accounts if account["followers"] >= min_followers]
+
+    if min_following is not None:
+        filtered_accounts = [account for account in filtered_accounts if account["following"] >= min_following]
+
+    if min_likes is not None:
+        filtered_accounts = [account for account in filtered_accounts if account["likes"] >= min_likes]
+
+    if user_id is not None:
+        filtered_accounts = [account for account in filtered_accounts if account["user_id"].lower() == user_id.lower()]
+
+    if username is not None:
+        filtered_accounts = [account for account in filtered_accounts if username.lower() in account["username"].lower()]
+
+    return filtered_accounts
+
+#건강관리 센터 회원 정보
+health_center_members = [
+    {"id": 1, "name": "John Doe", "gender": "M", "age": 30, "height": 180, "weight": 75, "blood_pressure": "120/80", "heart_rate": 70},
+    {"id": 2, "name": "Jane Smith", "gender": "F", "age": 45, "height": 165, "weight": 68, "blood_pressure": "130/85", "heart_rate": 75},
+    {"id": 3, "name": "David Johnson", "gender": "M", "age": 50, "height": 175, "weight": 80, "blood_pressure": "125/82", "heart_rate": 72},
+    {"id": 4, "name": "Emily Davis", "gender": "F", "age": 35, "height": 160, "weight": 55, "blood_pressure": "118/78", "heart_rate": 68},
+    {"id": 5, "name": "Michael Wilson", "gender": "M", "age": 55, "height": 170, "weight": 70, "blood_pressure": "130/90", "heart_rate": 78},
+    {"id": 6, "name": "Sarah Brown", "gender": "F", "age": 40, "height": 155, "weight": 58, "blood_pressure": "122/80", "heart_rate": 65},
+    {"id": 7, "name": "Christopher Lee", "gender": "M", "age": 60, "height": 175, "weight": 85, "blood_pressure": "135/88", "heart_rate": 75},
+    {"id": 8, "name": "Olivia Taylor", "gender": "F", "age": 50, "height": 165, "weight": 60, "blood_pressure": "120/80", "heart_rate": 70},
+    {"id": 9, "name": "Daniel Martin", "gender": "M", "age": 45, "height": 180, "weight": 78, "blood_pressure": "125/85", "heart_rate": 72},
+    {"id": 10, "name": "Sophia Anderson", "gender": "F", "age": 55, "height": 160, "weight": 65, "blood_pressure": "130/90", "heart_rate": 75}
+]
+
+@app.get("/health_center_members")
+async def filter_health_center_members(
+    name: str = Query(default=None),
+    gender: str = Query(default=None, regex="^[MF]$"),
+    age: int = Query(default=None, ge=0),
+    age_range: str = Query(default=None),
+    weight_range: str = Query(default=None),
+    height_range: str = Query(default=None)
+):
+    filtered_members = health_center_members
+
+    if name is not None:
+        filtered_members = [member for member in filtered_members if name.lower() in member["name"].lower()]
+
+    if gender is not None:
+        filtered_members = [member for member in filtered_members if member["gender"].lower() == gender.lower()]
+
+    if age is not None:
+        filtered_members = [member for member in filtered_members if member["age"] == age]
+
+    if age_range is not None:
+        age_min, age_max = map(int, age_range.split("-"))
+        filtered_members = [member for member in filtered_members if age_min <= member["age"] <= age_max]
+
+    if weight_range is not None:
+        weight_min, weight_max = map(int, weight_range.split("-"))
+        filtered_members = [member for member in filtered_members if weight_min <= member["weight"] <= weight_max]
+
+    if height_range is not None:
+        height_min, height_max = map(int, height_range.split("-"))
+        filtered_members = [member for member in filtered_members if height_min <= member["height"] <= height_max]
+
+    return filtered_members
+
+#약 처방 기록
+pharmacy_prescriptions = [
+    {"id": 1, "prescriber_age": 45, "prescriber_gender": "M", "medicine": "Medicine A", "prescription_date": "2022-01-01", "prescribing_doctor": "Dr. John Doe"},
+    {"id": 2, "prescriber_age": 35, "prescriber_gender": "F", "medicine": "Medicine B", "prescription_date": "2022-02-15", "prescribing_doctor": "Dr. Jane Smith"},
+    {"id": 3, "prescriber_age": 50, "prescriber_gender": "M", "medicine": "Medicine C", "prescription_date": "2022-03-10", "prescribing_doctor": "Dr. David Johnson"},
+    {"id": 4, "prescriber_age": 40, "prescriber_gender": "F", "medicine": "Medicine A", "prescription_date": "2022-04-05", "prescribing_doctor": "Dr. Emily Davis"},
+    {"id": 5, "prescriber_age": 55, "prescriber_gender": "M", "medicine": "Medicine B", "prescription_date": "2022-05-20", "prescribing_doctor": "Dr. Michael Wilson"},
+    {"id": 6, "prescriber_age": 30, "prescriber_gender": "F", "medicine": "Medicine C", "prescription_date": "2022-06-15", "prescribing_doctor": "Dr. Sarah Brown"},
+    {"id": 7, "prescriber_age": 60, "prescriber_gender": "M", "medicine": "Medicine A", "prescription_date": "2022-07-01", "prescribing_doctor": "Dr. Christopher Lee"},
+    {"id": 8, "prescriber_age": 50, "prescriber_gender": "F", "medicine": "Medicine B", "prescription_date": "2022-08-12", "prescribing_doctor": "Dr. Olivia Taylor"},
+    {"id": 9, "prescriber_age": 45, "prescriber_gender": "M", "medicine": "Medicine C", "prescription_date": "2022-09-05", "prescribing_doctor": "Dr. Daniel Martin"},
+    {"id": 10, "prescriber_age": 55, "prescriber_gender": "F", "medicine": "Medicine A", "prescription_date": "2022-10-20", "prescribing_doctor": "Dr. Sophia Anderson"}
+]
+
+@app.get("/pharmacy_prescriptions")
+async def filter_pharmacy_prescriptions(
+    age: int = Query(default=None, ge=0),
+    gender: str = Query(default=None, regex="^[MF]$"),
+    medicine: str = Query(default=None),
+    prescription_date: str = Query(default=None),
+):
+    filtered_prescriptions = pharmacy_prescriptions
+
+    if age is not None:
+        filtered_prescriptions = [prescription for prescription in filtered_prescriptions if prescription["prescriber_age"] == age]
+
+    if gender is not None:
+        filtered_prescriptions = [prescription for prescription in filtered_prescriptions if prescription["prescriber_gender"].lower() == gender.lower()]
+
+    if medicine is not None:
+        filtered_prescriptions = [prescription for prescription in filtered_prescriptions if medicine.lower() in prescription["medicine"].lower()]
+
+    if prescription_date is not None:
+        filtered_prescriptions = [prescription for prescription in filtered_prescriptions if prescription["prescription_date"] == prescription_date]
+
+    return filtered_prescriptions
+
+#스트리밍 영상 조회
+
+streaming_videos = [
+    {"title": "Video A", "channel": "Channel X", "views": 100000, "likes": 5000, "comments": 100, "shares": 50, "duration": "05:30"},
+    {"title": "Video B", "channel": "Channel Y", "views": 50000, "likes": 2000, "comments": 50, "shares": 20, "duration": "10:45"},
+    {"title": "Video C", "channel": "Channel X", "views": 80000, "likes": 3000, "comments": 80, "shares": 30, "duration": "03:15"},
+    {"title": "Video D", "channel": "Channel Z", "views": 120000, "likes": 8000, "comments": 200, "shares": 100, "duration": "08:20"},
+    {"title": "Video E", "channel": "Channel Y", "views": 60000, "likes": 2500, "comments": 70, "shares": 40, "duration": "06:50"},
+    {"title": "Video F", "channel": "Channel X", "views": 90000, "likes": 4000, "comments": 120, "shares": 60, "duration": "07:30"},
+    {"title": "Video G", "channel": "Channel Z", "views": 150000, "likes": 10000, "comments": 300, "shares": 150, "duration": "12:15"},
+    {"title": "Video H", "channel": "Channel Y", "views": 70000, "likes": 3500, "comments": 90, "shares": 35, "duration": "04:40"},
+    {"title": "Video I", "channel": "Channel X", "views": 110000, "likes": 6000, "comments": 150, "shares": 75, "duration": "09:05"},
+    {"title": "Video J", "channel": "Channel Z", "views": 100000, "likes": 5000, "comments": 100, "shares": 50, "duration": "06:10"}
+]
+
+@app.get("/streaming_videos")
+async def filter_streaming_videos(
+    title: str = Query(...),
+    channel: str = Query(default=None),
+    min_views: int = Query(default=None, ge=0),
+    min_likes: int = Query(default=None, ge=0),
+    duration_start: str = Query(default=None, regex="^\d{2}:\d{2}$"),
+    duration_end: str = Query(default=None, regex="^\d{2}:\d{2}$")
+):
+    if title:
+        filtered_videos = [video for video in streaming_videos if title.lower() in video["title"].lower()]
+
+    if channel is not None:
+        filtered_videos = [video for video in filtered_videos if channel.lower() == video["channel"].lower()]
+
+    if min_views is not None:
+        filtered_videos = [video for video in filtered_videos if video["views"] >= min_views]
+
+    if min_likes is not None:
+        filtered_videos = [video for video in filtered_videos if video["likes"] >= min_likes]
+
+    if duration_start and duration_end:
+        filtered_videos = [video for video in filtered_videos if duration_start <= video["duration"] <= duration_end]
+
+    return filtered_videos
+
+# 가상의 스포츠 경기 일정
+sports_schedule = [
+    {"date": "2023-06-01", "time": "14:00", "sport": "Football", "league": "Premier League", "home_team": "Manchester United", "away_team": "Chelsea", "venue": "Old Trafford"},
+    {"date": "2023-06-01", "time": "17:30", "sport": "Football", "league": "La Liga", "home_team": "Real Madrid", "away_team": "Barcelona", "venue": "Santiago Bernabeu"},
+    {"date": "2023-06-02", "time": "19:00", "sport": "Football", "league": "Bundesliga", "home_team": "Bayern Munich", "away_team": "Borussia Dortmund", "venue": "Allianz Arena"},
+    {"date": "2023-06-02", "time": "20:30", "sport": "Football", "league": "Serie A", "home_team": "Juventus", "away_team": "AC Milan", "venue": "Allianz Stadium"},
+    {"date": "2023-06-03", "time": "15:00", "sport": "Football", "league": "Ligue 1", "home_team": "Paris Saint-Germain", "away_team": "Olympique Marseille", "venue": "Parc des Princes"},
+    {"date": "2023-06-03", "time": "18:00", "sport": "Football", "league": "Eredivisie", "home_team": "Ajax", "away_team": "PSV Eindhoven", "venue": "Johan Cruyff Arena"},
+    {"date": "2023-06-04", "time": "13:30", "sport": "Football", "league": "Premier League", "home_team": "Liverpool", "away_team": "Manchester City", "venue": "Anfield"},
+    {"date": "2023-06-04", "time": "16:00", "sport": "Football", "league": "La Liga", "home_team": "Atletico Madrid", "away_team": "Sevilla", "venue": "Wanda Metropolitano"},
+    {"date": "2023-06-05", "time": "20:00", "sport": "Football", "league": "Bundesliga", "home_team": "RB Leipzig", "away_team": "Bayer Leverkusen", "venue": "Red Bull Arena"},
+    {"date": "2023-06-05", "time": "21:30", "sport": "Football", "league": "Serie A", "home_team": "Inter Milan", "away_team": "Napoli", "venue": "San Siro"}
+]
+
+@app.get("/sports_schedule")
+async def filter_sports_schedule(
+    date: str = Query(default=None),
+    sport: str = Query(default=None),
+    team: str = Query(default=None),
+    league: str = Query(default=None)
+):
+    filtered_schedule = sports_schedule
+
+    if date is not None:
+        filtered_schedule = [game for game in filtered_schedule if game["date"] == date]
+
+    if sport is not None:
+        filtered_schedule = [game for game in filtered_schedule if sport.lower() in game["sport"].lower()]
+
+    if team is not None:
+        filtered_schedule = [game for game in filtered_schedule if team.lower() in game["home_team"].lower() or team.lower() in game["away_team"].lower()]
+
+    if league is not None:
+        filtered_schedule = [game for game in filtered_schedule if league.lower() in game["league"].lower()]
+
+    return filtered_schedule
+
